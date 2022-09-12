93ff0bf35d9eb5596d84d8df837653dccbb332ec
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-09-12 20:56:14 +0300

    feat: :sparkles: drawing grid and created html

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Time to script!
+const buttons = document.querySelectorAll('.buttons');
+const board = document.querySelector('.board');
+
+let pixels = '';
+let screen = 16
+
+const drawGrid = (screen) => {
+  for (i = 0; i < screen ** 2 ; i++){
+    pixels = document.createElement('div');
+    pixels.classList.add('pixels');
+    pixels.style.backgroundColor = 'white';
+    board.appendChild(pixels);
+  }
+
+  board.style.gridTemplateColumns = `repeat(${screen}, auto)`;
+  board.style.gridTemplateRows = `repeat(${screen}, auto)`;
+}
+
+drawGrid(screen);
