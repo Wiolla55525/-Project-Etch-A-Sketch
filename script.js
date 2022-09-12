78b278abe41d156fbcd5d01e164fc3ee30b206b2
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-09-12 21:41:35 +0300

    feat: :art: create a new look for page

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const buttons = document.querySelectorAll('.buttons');
-const board = document.querySelector('.board');
+const buttons = document.querySelectorAll(".buttons");
+const board = document.querySelector(".board");
 
-let pixels = '';
-let screen = 16
+let pixels = "";
+let screen = 16;
 
 const drawGrid = (screen) => {
-  for (i = 0; i < screen ** 2 ; i++){
-    pixels = document.createElement('div');
-    pixels.classList.add('pixels');
-    pixels.style.backgroundColor = 'white';
+  for (i = 0; i < screen ** 2; i++) {
+    pixels = document.createElement("div");
+    pixels.classList.add("pixels");
+    pixels.style.backgroundColor = "white";
     board.appendChild(pixels);
   }
 
   board.style.gridTemplateColumns = `repeat(${screen}, auto)`;
   board.style.gridTemplateRows = `repeat(${screen}, auto)`;
-}
+};
 
 drawGrid(screen);
