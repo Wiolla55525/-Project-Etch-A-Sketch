f9d810b8f96896d9c34b8ce327196e93501e0fb6
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-09-19 19:46:12 +0300

    refactor: :poop:

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let pixels = "";
 let screen = 16;
 
 const drawGrid = (screen) => {
-  for (i = 0; i < screen ** 2; i++) {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < screen ** 2; i++) {
     pixels = document.createElement("div");
     pixels.classList.add("pixels");
     pixels.style.backgroundColor = "white";
@@ -34,8 +35,8 @@ drawGrid(screen);
 // }
 
 const colorOnHover = () => {
-  let pixels = document.querySelectorAll('.pixels');
-  pixels.forEach(pixel => {
+  let pixelsColor = document.querySelectorAll('.pixels');
+  pixelsColor.forEach(pixel => {
     pixel.addEventListener('mouseover', (e) => {
       e.target.style.backgroundColor = 'pink';
     })
